6d89ab1d70339c1a54e1ffedf29770a772c09495
Vo-Van-Mai <136545831+Vo-Van-Mai@users.noreply.github.com>
2023-08-10 03:17:23 +0700

    update

--- javascript/JavaScript.js
+++ javascript/JavaScript.js
@@ -1,6 +1,5 @@
-var h1 = document.querySelectorAll('h1');
-for (var i = 0; i < h1.length; i++) {
-    h1[i].onclick = function(e) {
-        console.log(e.target);
-    }
+var inputElement =
+ document.querySelector('input[type="text"]');
+inputElement.onkeyup= function(e) {
+    console.log(e.which);
 }
